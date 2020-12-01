343c74c2344ad55c2b90ede31b018ad8daee0b74
amtesire <tesiregisele@gmail.com>
2020-12-01 14:32:58 +0200

    removed unnecessary codes

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     __tablename__= 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique = True, index = True)
+    email = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
